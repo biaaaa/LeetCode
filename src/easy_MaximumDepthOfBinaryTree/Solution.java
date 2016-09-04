fcd5aa30c7b47a66152ef3548302caf2c1e4ae25
Tianyu Lu <lu.ti@husky.neu.edu>
2016-09-04 13:03:02 -0400

    new

--- src/easy_MaximumDepthOfBinaryTree/Solution.java
+++ src/easy_MaximumDepthOfBinaryTree/Solution.java
@@ -1,6 +1,10 @@
 package easy_MaximumDepthOfBinaryTree;
 
 /**
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path from the root node 
+ * down to the farthest leaf node.
+ * 
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
